88149cf48f67a1a7e3565f69d2a5f395c6844148
Ansh Raiyani <anshraiyani13@gmail.com>
2023-05-24 22:09:28 +0530

    added myName.js file

--- full-stack-javascript/javascript/webpack/src/index.js
+++ full-stack-javascript/javascript/webpack/src/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash'
+import myName from './myName'
 
 function component() {
   const element = document.createElement('div');
 
   //lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.textContent=myName('Ansh')
 
   return element;
 }
