6f8dc89c65227b0784ea731661e5332c3a233ce2
Ansh Raiyani <anshraiyani13@gmail.com>
2023-06-08 17:55:34 +0530

    added states files

---
+++ full-stack-javascript/javascript/react-js/states/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";  
+
+class App extends Component{
+    constructor(){
+        super()
+        this.state={
+            count:0
+        }
+        this.countUp=this.countUp.bind(this)
+    }
+
+    countUp(){
+        this.setState({
+            count: this.state.count+1
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.countUp}>Click me</button>
+                <p>{this.state.count}</p>
+            </div>
+        )
+    }
+}
+
+export default App
