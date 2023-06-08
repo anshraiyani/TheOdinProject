3520bba89cd618494f0e85d779313eae9bfee7c3
Ansh Raiyani <anshraiyani13@gmail.com>
2023-06-08 18:18:13 +0530

    added  code

--- full-stack-javascript/javascript/react-js/props/src/App.js
+++ full-stack-javascript/javascript/react-js/props/src/App.js
@@ -1,23 +1,36 @@
 import React, { Component } from "react";
 import MyComponent from "./components/MyComponent";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.onClickBtn=this.onClickBtn.bind(this)
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.onClickBtn=this.onClickBtn.bind(this)
+//   }
+
+//   onClickBtn(){
+//     console.log("button clicked")
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <MyComponent title="React" onButtonClicked={this.onClickBtn}/>
+//       </div>
+//     );
+//   }
+// }
+
+function App(){
+
+  function someFunction() {
+    console.log('you clicked the button')
   }
 
-  onClickBtn(){
-    console.log("button clicked")
-  }
-
-  render() {
-    return (
-      <div>
-        <MyComponent title="React" onButtonClicked={this.onClickBtn}/>
-      </div>
-    );
-  }
+  return(
+    <div>
+      <MyComponent title="hello" fun={someFunction}/>
+    </div>
+  )
 }
 
 export default App;
--- full-stack-javascript/javascript/react-js/props/src/components/MyComponent.js
+++ full-stack-javascript/javascript/react-js/props/src/components/MyComponent.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 
-class MyComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
+// USING CLASS COMPONENTS
 
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <button onClick={this.props.onButtonClicked}>Click me</button>
-      </div>
-    );
-  }
+// class MyComponent extends Component {
+//   constructor(props) {
+//     super(props);
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>{this.props.title}</h1>
+//         <button onClick={this.props.onButtonClicked}>Click me</button>
+//       </div>
+//     );
+//   }
+// }
+
+//USING FUNCTIONAL COMPONENTS
+
+function MyComponent(props){
+    return(
+        <button onClick={()=>props.fun()}>
+            {props.title}
+        </button>
+    )
 }
 
 export default MyComponent;
