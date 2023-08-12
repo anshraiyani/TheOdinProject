a69010e6a92c139363292baac683e9fff760560e
Ansh Raiyani <anshraiyani13@gmail.com>
2023-08-12 19:15:32 +0530

    updated http-module folder

--- full-stack-javascript/node-js/getting-started/http-module/server.js
+++ full-stack-javascript/node-js/getting-started/http-module/server.js
@@ -1,22 +1,36 @@
-const https = require('https');
+const http = require('http');
+const fs=require('fs')
 
-const options = {
-  hostname: 'example.com',
-  port: 443,
-  path: '/todos',
-  method: 'GET',
-};
+// const options = {
+//   hostname: 'example.com',
+//   port: 443,
+//   path: '/todos',
+//   method: 'GET',
+// };
 
-const req = https.request(options, res => {
-  console.log(`statusCode: ${res.statusCode}`);
+// const req = https.request(options, res => {
+//   console.log(`statusCode: ${res.statusCode}`);
 
-  res.on('data', d => {
-    process.stdout.write(d);
-  });
-});
+//   res.on('data', d => {
+//     process.stdout.write(d);
+//   });
+// });
 
-req.on('error', error => {
-  console.error(error);
-});
+// req.on('error', error => {
+//   console.error(error);
+// });
 
-req.end();
+// req.end();
+
+const data=fs.readFileSync('data.json','utf-8')
+const index=fs.readFileSync('index.html','utf-8')
+
+const server=http.createServer((req,res)=>{
+  console.log('server started')
+  res.setHeader('dummy','dummyValue')
+  res.setHeader('Content-Type','application/json')
+  res.setHeader('Content-Type','text/html')
+  res.end(index)
+})
+
+server.listen(8080)
