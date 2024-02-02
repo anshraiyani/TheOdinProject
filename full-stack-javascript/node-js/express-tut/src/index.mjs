fd1eecfa7dcc1fd04334706e6d0a277b0054f83b
Ansh Raiyani <anshraiyani13@gmail.com>
2024-02-02 22:31:32 +0530

    complete passport.js

--- full-stack-javascript/node-js/express-tut/src/index.mjs
+++ full-stack-javascript/node-js/express-tut/src/index.mjs
@@ -2,7 +2,6 @@ import express from "express";
 import indexRouter from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
-import { mockUsers } from "./utils/constants.mjs";
 import passport from "passport";
 import "./strategies/local-strategies.mjs";
 
@@ -25,61 +24,25 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(indexRouter);
 
-const loggingMiddleWare = (req, res, next) => {
-  console.log(`${req.method} - ${req.url}`);
-  next();
-};
-
-// app.use(loggingMiddleWare); //register middleware globally
-
-const PORT = process.env.PORT || 3000;
-
-app.get("/", loggingMiddleWare, (req, res) => {
-  console.log(req.session);
-  console.log(req.session.id);
-  req.session.visited = true;
-  res.cookie("key", "value", { maxAge: 60000 * 60, signed: true });
-  res.status(201).send({ msg: "hello" });
-});
-
-app.post("/api/auth", (req, res) => {
-  const {
-    body: { username, password },
-  } = req;
-  const findUser = mockUsers.find((user) => user.username === username);
-  if (!findUser || findUser.password !== password) {
-    return res.status(401).send({ msg: "BAD CREDENTIALS" });
-  }
-
-  req.session.user = findUser;
-  return res.status(200).send(findUser);
+app.post("/api/auth", passport.authenticate("local"), (req, res) => {
+  res.sendStatus(200);
 });
 
 app.get("/api/auth/status", (req, res) => {
-  return req.session.user
-    ? res.status(200).send(req.session.user)
-    : res.status(401).send({ msg: "NOT AUTHENTICATED" });
+  console.log("inside /api/auth/status");
+  console.log(req.user);
+  return req.user ? res.send(req.user) : res.sendStatus(401);
 });
 
-app.post("/api/cart", (req, res) => {
-  if (!req.session.user)
-    return res.status(401).send({ msg: "NOT AUTHENTICATED" });
-  const { body: item } = req;
-  const { cart } = req.session;
-  if (cart) {
-    cart.push(item);
-  } else {
-    req.session.cart = [item];
-  }
-  return res.status(201).send(item);
+app.post("/api/auth/logout", (req, res) => {
+  if (!req.user) return res.sendStatus(401);
+  req.logOut((err) => {
+    if (err) return res.sendStatus(400);
+    res.sendStatus(200);
+  });
 });
 
-app.get("/api/cart", (req, res) => {
-  if (!req.session.user)
-    return res.status(401).send({ msg: "NOT AUTHENTICATED" });
-  return res.send(req.session.cart ?? []);
-});
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`server started in port: ${PORT}`);
 });
--- full-stack-javascript/node-js/express-tut/src/strategies/local-strategies.mjs
+++ full-stack-javascript/node-js/express-tut/src/strategies/local-strategies.mjs
@@ -2,6 +2,24 @@ import passport from "passport";
 import { Strategy } from "passport-local";
 import { mockUsers } from "../utils/constants.mjs";
 
+passport.serializeUser((user, done) => {
+  console.log("inside serialized user");
+  console.log(user);
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  console.log("inside deserialize");
+  console.log(`deserialized id: ${id}`);
+  try {
+    const findUser = mockUsers.find((user) => user.id === id);
+    if (!findUser) throw new Error("user not found");
+    done(null, findUser);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
 export default passport.use(
   new Strategy((username, password, done) => {
     console.log(`username: ${username}`);
